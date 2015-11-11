557203f1389c3c11e557ba54e7f7d720af6b24b4
Karl Campanella-Dysart <karlcd@me.com>
2015-11-10 22:19:22 -0800

    make it better

--- app.js
+++ app.js
@@ -32,4 +32,5 @@ app.get('/miles', function(req, res) {
 });
 
 //listen
-app.listen(3000, function() { console.log('Listening on port 3000'); });
+var port = process.env.PORT || 3000;
+app.listen(port, function() { console.log('Listening on port 3000'); });
